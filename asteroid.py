279319eae155c09840dfb435602de510e1d29b2b
Ab-dell <hakemabdel59@gmail.com>
2024-09-23 23:53:06 +0200

    add asteroids to the game

--- asteroid.py
+++ asteroid.py
@@ -3,9 +3,8 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
+        super().__init__(x, y, radius)
+        
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init() #initialize pygame
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #create a GUI to display the game
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     clock = pygame.time.Clock()
     dt = 0
+    
 
     while True:
         #Stop the game if the GUI is closed
@@ -16,12 +26,18 @@ def main():
                 return
             
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for item in updatable:
+            item.update(dt)
+            
+        for item in drawable:
+            item.draw(screen)
+
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
         
         
-        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
