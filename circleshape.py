e4a4bf94ba6aae1487de1222f7e216044164d100
HAKEM Abdeldjebar <abdeldjebar.hakem@hubone.fr>
2024-09-24 10:29:55 +0200

    simplify the check_for_collision method

--- circleshape.py
+++ circleshape.py
@@ -22,7 +22,5 @@ class CircleShape(pygame.sprite.Sprite):
         pass
 
     def check_for_collision(self, other):
-        if self.position.distance_to(other.position) <= self.radius + other.radius:
-            return True
-        else:
-            return False
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+            
